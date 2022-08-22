20a3d7cbc3f0c1deed93268ae857c5b331812fb9
serhantopuz <serhantpz123@gmail.com>
2022-08-22 23:30:56 +0300

    final

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 word = input("Enter a word: ")
 
-phonetic_list = [new_dict[letter] for letter in word.upper() if letter in new_dict.keys()]
+phonetic_list = [new_dict[letter] for letter in word.upper()]
 print(phonetic_list)
