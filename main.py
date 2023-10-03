aafd74564264651ce007b9f420a76f940659d404
serhantopuz <serhantpz123@gmail.com>
2023-10-03 22:37:20 +0300

    error handling

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ")
 
-phonetic_list = [new_dict[letter] for letter in word.upper()]
-print(phonetic_list)
+def generate_phonetic():
+    word = input("Enter a word: ")
+    try:
+        phonetic_list = [new_dict[letter] for letter in word.upper()]
+    except KeyError:
+        print("Sorry, only the letters in the alphabet please.\n")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
+
+
+generate_phonetic()
+
